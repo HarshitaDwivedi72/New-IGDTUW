bb9c30d0fef18771626948a80ab39e77d8bcf236
tanyagarg2509 <tanyagarg2509@gmail.com>
2019-11-11 23:02:58 +0530

    computerScience.php file added

---
+++ computerScience.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Applied Science and Humanities</title>
+
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=EB+Garamond:500|Roboto&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" id="stylesheet" href="stylesheet/style1.css">
+    <link rel="stylesheet" id="stylesheet" href="stylesheet/computerScience.css">
+
+    
+</head>
+
+<body>
+   <!-- header -->
+   <?php include 'header.php';?>
+
+<!-- Main Body -->
+<div class="container">
+        <div class="row">
+            <div class="col-sm-3">
+                <div class="sideNav">
+                    <div class="sideHeading">
+                        Computer Science & Engineering
+                    </div>
+                    <ul class="sidenav">
+                        <li><a href="">Objectives</a></li>
+                        <li><a href="">Head Of Department</a></li>
+                        <li><a href="">Faculty Directory</a></li>
+                        <li><a href="">Labs</a></li>
+                        <li><a href="">Events/Workshops/Seminars</a></li>
+                        <li><a href="">Achievements</a></li>
+                        <li><a href="">Expert Lectures</a></li>
+                        
+                    </ul>
+
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="box-1">
+                    <div class="title">
+                        DEPARTMENT OF CSE
+                    </div>
+                    <div class="headingPara">
+                        The Computer Science and Engineering Department is the most versatile department of college and caters to the needs of other departments as well. The department aims at developing the fundamental conceptual knowledge along with the total personality, which helps computer engineers to face the challenges of rapidly changing software industry. Technical skills, teamwork and specialized knowledge prepare these young engineers to adopt and continuously learn new technologies. Regular updation and expansion of the labs is done as per the requirement of the industry. Exposure to latest developments and technologies is a mandatory exercise for both faculty and students. Continuous research work and live projects are taken up by the department. Numerous professional activities are conducted to improve interaction between students, faculty, other academic institutions and industries.
+                       
+                    </div>
+
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="sideNav">
+                    <div class="sideHeading">
+                        QUICK LINKS - CSE
+                    </div>
+                    <ul class="sidenav">
+                        <li><a href="">Syllabus</a></li>
+                        
+                    </ul>
+
+
+                    <ul class="timeTable">
+                        <li class="ttHeading">Time Table</li>
+                        <br>
+                        <li><a href="">B.Tech</a></li>
+                        <li><a href="">M.Tech</a></li>
+                        <li><a href="">Lab Manuals</a></li>
+                        
+                    </ul>
+                </div>
+                <div class="sideNav">
+                    <div class="sideHeading">
+                        EVENTS
+                    </div>
+                    <ul class="sidenav">
+                        <li><a href="">7th Summer Training Program on Build Your Own IoT Enabled Smart Device from 06-06-2016 to 15-07-2016 in collaboration with Microsoft University Relations.</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+   
+   <!-- footer -->
+    <?php include 'footer.php';?>
+</body>
+
+</html>
